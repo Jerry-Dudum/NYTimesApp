4f19b24b9f18c3ad748cdce01a33b4d33b84dfcb
Jerry Dudum <jsdudum@gmail.com>
2019-09-11 10:07:37 -0700

    linked js to html to test any bugs between front end and backend

---
+++ app.js
@@ -0,0 +1,58 @@
+// Event listener of id of cat-button
+$("#search-button").on("click", function (event) {
+    event.preventDefault();
+    var search = $("#search-input").val().trim();
+    var start = $("#start-input").val().trim();
+    var end = $("#end-input").val().trim();
+    var numberOfRecords = $("#number-input").val().trim();
+    // An URL string assigned to queryURL
+    if (start === "" && end === "") {
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=Y8Pp6fak747GzhErHVymE1ceC4GIzjox";
+
+    }
+    else {
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=Y8Pp6fak747GzhErHVymE1ceC4GIzjox&facet_fields=source&facet=true&begin_date=" + start + "&end_date=" + end;
+    }
+    // AJAX method that requests to queryURL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+        // the response is got upon request
+        .then(function (response) {
+            console.log(response);
+            var articles = response.response.docs;
+
+            // console.log(articles[4]);
+            for (var i = 0; i < numberOfRecords; i++) {
+                console.log(articles[i].headline.print_headline);
+                console.log(articles[i].snippet);
+                console.log(articles[i].byline.original);
+                var newDiv = $("<div>");
+                newDiv.addClass("article");
+                var headline = articles[i].headline.print_headline;
+                var snippet = articles[i].snippet;
+                var original = articles[i].byline.original;
+                var link = articles[i].web_url;
+
+                newDiv.append("<p>" + headline + "</p>");
+                newDiv.append("<p>" + snippet + "</p>");
+                newDiv.append("<p>" + original + "</p>");
+                // newDiv.append("<a href=" + link + "</a>");
+                newDiv.append($("<a>").attr("href", link).text("Go To Article"));
+
+                $("#articles").append(newDiv);
+
+            }
+            $("#clear-button").on("click", function () {
+
+                $("#search-input").val("");
+                $("#number-input").val("");
+                $("#start-input").val("");
+                $("#end-input").val("");
+                $("#articles").empty();
+            });
+
+        });
+});
